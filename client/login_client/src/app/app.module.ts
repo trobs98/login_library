36f08a15425823778ca5322a41d54e6643872238
trobs98 <tylergordonr@gmail.com>
2022-12-15 17:25:29 -0500

    Added template auth service, and login and signup components, added basic login form, added bootstrap to project

--- client/login_client/src/app/app.module.ts
+++ client/login_client/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/login_client/src/app/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.maxLength(100), Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onLogin() {
+
+  }
+
+  get emailControls() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordControls() {
+    return this.loginForm.get('password');
+  }
+
+  printLogin() {
+    console.log('form controls: ', this.loginForm);
+    console.log('passwordControls: ', this.passwordControls);
+  }
+
+}
