de0c954169e989ad95e1e2678b371b0ee9816f2f
trobs98 <tylergordonr@gmail.com>
2023-01-10 18:07:46 -0500

    Updating user schema

--- schema/user_schema.sql
+++ schema/user_schema.sql
@@ -1,48 +1,49 @@
+CREATE DATABASE  IF NOT EXISTS `UserSchema`
+USE `UserSchema`;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema UserSchema
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `UserSchema` ;
-USE `UserSchema` ;
-
--- -----------------------------------------------------
--- Table `UserSchema`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `UserSchema`.`User` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(100) NOT NULL,
-  `last_name` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `hash_password` VARCHAR(344) NOT NULL,
-  `salt` VARCHAR(344) NOT NULL,
-  `create_date` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `UserSchema`.`UserAudit`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `UserSchema`.`UserAudit` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `FK_userId` INT NOT NULL,
-  `login_date` DATETIME NOT NULL,
-  `login_IP` VARCHAR(45) NOT NULL,
-  `cookie_hash` VARCHAR(100) NOT NULL,
-  `salt` VARCHAR(100) NOT NULL,
+--
+-- Table structure for table `ResetPasswordToken`
+--
+DROP TABLE IF EXISTS `ResetPasswordToken`;
+CREATE TABLE `ResetPasswordToken` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `FK_userId` int NOT NULL,
+  `hash_token` varchar(344) NOT NULL,
+  `salt` varchar(344) NOT NULL,
+  `expiration_date` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `FK_userId_idx` (`FK_userId` ASC) VISIBLE,
-  CONSTRAINT `FK_userId_UserAudit`
-    FOREIGN KEY (`FK_userId`)
-    REFERENCES `UserSchema`.`User` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `FK_userId_idx` (`FK_userId`),
+  CONSTRAINT `FK_ResetPassword_userId` FOREIGN KEY (`FK_userId`) REFERENCES `User` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Table structure for table `User`
+--
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `hash_password` varchar(344) NOT NULL,
+  `salt` varchar(344) NOT NULL,
+  `create_date` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Table structure for table `UserAudit`
+--
+DROP TABLE IF EXISTS `UserAudit`;
+CREATE TABLE `UserAudit` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `FK_userId` int NOT NULL,
+  `login_date` varchar(25) NOT NULL,
+  `login_IP` varchar(15) NOT NULL,
+  `cookie` varchar(250) NOT NULL,
+  `expiry_date` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_userId_UserAudit_idx` (`FK_userId`),
+  CONSTRAINT `FK_userId` FOREIGN KEY (`FK_userId`) REFERENCES `User` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
